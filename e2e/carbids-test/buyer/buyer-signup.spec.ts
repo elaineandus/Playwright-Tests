2f5a89fffd6cc6c8bd12c71dea393028c0fd4daa
elaineandus <elaineandus30@gmail.com>
2023-06-06 14:10:34 +0800

    buyer-signup

--- e2e/carbids-test/buyer/buyer-signup.spec.ts
+++ e2e/carbids-test/buyer/buyer-signup.spec.ts
@@ -10,7 +10,7 @@ test('buyer should be able to sign up successfully', async ({ page }) => {
 
   await page.getByPlaceholder('Enter your first name').fill('ariana');
   await page.getByPlaceholder('Enter your last name').fill('grande');
-  await page.getByRole('textbox', { name: 'Email address' }).fill('anon31@gmail.com');
+  await page.getByRole('textbox', { name: 'Email address' }).fill('anon30@gmail.com');
   
   await page.type('input[id="signup-password"]', 'password');
   await page.type('input[id="signup-password-confirm"]', 'password');  
